76edcd37d5d37b4187c80af81bb29445fe8df8e1
Jeremy Douglass <jeremydouglass@gmail.com>
2019-07-14 18:10:31 -0700

    setup_local: quick fix for local development using brew + virtualenv + python3 + TextMate

---
+++ setup_local.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# this is the virtual environment that .tm_properties for textmate points to
+# https://stackoverflow.com/a/48958409/7207622
+
+mkvirtualenv --python=/usr/local/bin/python3 archive_for_processing
